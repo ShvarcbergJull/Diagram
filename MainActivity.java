e0653aaa7c3729c7fae5f1d8ace453877904b064
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2019-12-19 21:24:21 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,10 +2,12 @@ package com.example.pc.circle_diagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     EditText et;
     boolean flagin = false;
     Diagram_View dv;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et = findViewById(R.id.parts);
         dv = findViewById(R.id.diagram);
+        tv = findViewById(R.id.procent);
+    }
+
+    public void setTv (String str) {
+        Log.d("Sectors", str);
+        tv.setText(str);
     }
 
     public void onClick(View v) {
-       String temp = et.getText().toString();
-       flagin = true;
-       dv.tester(temp);
+        if (v.getId() == R.id.created) {
+            String temp = et.getText().toString();
+            flagin = true;
+            dv.tester(temp);
+        }
     }
 }
